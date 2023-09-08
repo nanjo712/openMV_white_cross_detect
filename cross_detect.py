01485141b32b169d7220c350dbe5df2e78f3fc09
woshiren <yejiacong@outlook.com>
2023-09-08 20:47:17 +0800

    backup

--- cross_detect.py
+++ cross_detect.py
@@ -6,30 +6,31 @@ import math
 
 class DSU():
     def __init__(self,num):
-        father=[i for i in range(num)]
-        size=[1 for i in range(num)]
-        total=num
+        self.father=[i for i in range(num)]
+        self.size=[1 for i in range(num)]
+        self.total=num
     def find_father(self,v):
-        if father[v] != v :
-            return father[v] = self.find_father(father[v])
-        else
+        if self.father[v] != v :
+            self.father[v] = self.find_father(self.father[v])
+            return self.father[v]
+        else:
             return v
     def merge(self,x,y):
-        if check(x,y)：
+        if check(x,y):
             return None
         xx=self.find_father(x)
         yy=self.find_father(y)
-        if size[xx]>size[yy]:
+        if self.size[xx]>self.size[yy]:
             xx,yy=yy,xx
-        father[xx]=yy
-        size[yy]+=size[xx]
-        total-=1
+        self.father[xx]=yy
+        self.size[yy]+=size[xx]
+        self.total-=1
     def check(self,x,y):
         xx=self.find_father(x)
         yy=self.find_father(y)
-        if father[x] == father[y]:
+        if self.father[x] == self.father[y]:
             return True
-        else
+        else:
             return False
 
 
@@ -90,42 +91,61 @@ while True:
     rhos = [i.rho() for i in lines]
     thetas = [i.theta() for i in lines]
 
-    dsu=DSU(lines.size())
-    for i in range(lines.size()):
-        for j in range(lines.size()):
+    dsu=DSU(len(lines))
+    for i in range(len(lines)):
+        for j in range(len(lines)):
             if not dsu.check(i,j):
                 if getEuclideanDistance([rhos[i],thetas[i]],[rhos[j],thetas[j]])<1:
                     dsu.merge(i,j)
 
-    tags=[False for i in range(lines.size())]
+    tags=[False for i in range(len(lines))]
     sets=[]
-    for i in range(lines.size()):
+    for i in range(len(lines)):
         if tags[dsu.find_father(i)] == True :
             continue
-        sets.append([dsu.find_father(i),dsu.size[dsu.find_father[i]]])
+        sets.append([dsu.find_father(i),dsu.size[dsu.find_father(i)]])
 
     quick_sort(sets,compare_func = lambda a, b: b[1] - a[1])
-    if sets.size()<4:
+    if len(sets) != 4:
         pass
     else:
         four_large_set = [ sets[i] for i in range(4) ]
-        four_lines_set = []
+        four_lines_set = [[],[],[],[]]
         cnt = -1
         for i in four_large_set:
             cnt += 1
-            for j in range(lines.size()):
+            for j in range(len(lines)):
                 if dsu.find_father(j)==i[0]:
-                    four_line_set[cnt].append([rhos[j],thetas[j]])
+                    four_lines_set[cnt].append([rhos[j],thetas[j]])
         cnt = -1
         avg_lines = []
-        for i in four_line_set:
+        for i in four_lines_set:
             cnt += 1
             avg_rho=sum([i[j][0] for j in range(len(i))])/len(i)
             avg_theta=sum([i[j][1] for j in range(len(i))])/len(i)
-            avg_lines.append()
-        # TODO
-        # 生成一般式，所有边缘点匹配四条直线，
-        # 然后对边缘的点做线性回归得四条构成十字的直线
-        # 由此得到十字的直线解析式，据此计算十字中心
+
+            theta_rad = math.radians(avg_theta)
+            A = -math.sin(theta_rad)
+            B = math.cos(theta_rad)
+            C = avg_rho
+
+            avg_lines.append([A,B,C])
+        points_class = [[],[],[],[]]
+        for i in range(img.width()):
+            for j in range(img.height()):
+                if img.get_pixel(i,j) == 255:
+                    mindis = 0x3f3f3f3f
+                    minrec = -1
+                    for k in range(len(avg_lines)):
+                        curdis = (avg_lines[k][0]*i+avg_lines[k][1]*j+avg_lines[k][2])/((avg_lines[k][0]**2+avg_lines[k][1]**2)**0.5)
+                        if curdis < mindis:
+                            mindis = curdis
+                            minrec = k
+                    points_class[minrec].append([i,j])
+
+
+
+
+
 
     print(clock.fps())
